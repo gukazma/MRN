22419b0ab75b74c4c2d8226fceceafd7cfee2d54
geekgary-x <18017158729@163.com>
2023-07-20 17:38:54 +0800

    🐞 fix: osgb to mvsmesh

--- Modules/Mesh/Sources/MRN/Mesh/Implement/MeshImplBase.cpp
+++ Modules/Mesh/Sources/MRN/Mesh/Implement/MeshImplBase.cpp
@@ -7,6 +7,13 @@ void MeshImplBase::write(const boost::filesystem::path& path_) {
     std::ofstream f(path_.generic_string());
     VertexColorMap vpmap;
     bool           created;
+
+    for (auto v : CGAL::vertices(m_nativeMesh)) {
+        if (m_nativeMesh.is_border(v)) {
+            std::cout << "is border: " << v << std::endl;
+        }
+    }
+
     boost::tie(vpmap, created) = m_nativeMesh.property_map<VertexIndex, CGAL::IO::Color>("v:color");
     if (path_.extension() == ".ply")
         CGAL::IO::write_PLY(f, m_nativeMesh, CGAL::parameters::vertex_color_map(vpmap));
--- Modules/Mesh/Sources/MRN/Mesh/Implement/OSGB/OSGBMeshImpl.cpp
+++ Modules/Mesh/Sources/MRN/Mesh/Implement/OSGB/OSGBMeshImpl.cpp
@@ -31,7 +31,7 @@ public:
                 dynamic_cast<osg::Vec2Array*>(geom->getTexCoordArray(0));
             // get vertex
             osg::Vec3Array* vertices = dynamic_cast<osg::Vec3Array*>(geom->getVertexArray());
-            for (size_t i = 0; i < vertices->size(); i++) {
+            /*for (size_t i = 0; i < vertices->size(); i++) {
                 auto v = vertices->at(i);
                 Point3      p = Point3(v.x(), v.y(), v.z());
                 VertexIndex vertex_index = m_nativeMeshPreference.add_vertex(p);
@@ -41,7 +41,7 @@ public:
                 vcmap[vertex_index]      =  CGAL::IO::Color(image->getColor(coord).r()*255,
                                                       image->getColor(coord).g() * 255,
                                                       image->getColor(coord).b() * 255);
-            }
+            }*/
 
             auto faces_uint   = dynamic_cast<osg::DrawElementsUInt*>(geom->getPrimitiveSet(0));
             auto faces_ushort = dynamic_cast<osg::DrawElementsUShort*>(geom->getPrimitiveSet(0));
@@ -62,9 +62,41 @@ public:
                     faces.push_back(faces_ushort->getElement(i));
                 }
             }
+            std::map<Point3, VertexIndex> pvmap;
+            
             for (size_t i = 0; i < faces.size(); i+=3) {
-                FaceIndex face_index = m_nativeMeshPreference.add_face(
-                    VertexIndex(faces[i]), VertexIndex(faces[i+1]), VertexIndex(faces[i+2]));
+                auto        v0 = vertices->at(faces[i]);
+                auto        v1 = vertices->at(faces[i+1]);
+                auto        v2 = vertices->at(faces[i+2]);
+                Point3      p0 = Point3(v0.x(), v0.y(), v0.z());
+                Point3      p1 = Point3(v1.x(), v1.y(), v1.z());
+                Point3      p2 = Point3(v2.x(), v2.y(), v2.z());
+                
+
+                if (pvmap.find(p0) == pvmap.end())
+                {
+                    pvmap[p0] = m_nativeMeshPreference.add_vertex(p0);
+                    auto coord       = textureCoordArray->at(faces[i]);
+                    vcmap[pvmap[p0]] = CGAL::IO::Color(image->getColor(coord).r() * 255,
+                                                          image->getColor(coord).g() * 255,
+                                                          image->getColor(coord).b() * 255);
+                }
+                if (pvmap.find(p1) == pvmap.end()) {
+                    pvmap[p1]        = m_nativeMeshPreference.add_vertex(p1);
+                    auto coord       = textureCoordArray->at(faces[i+1]);
+                    vcmap[pvmap[p1]] = CGAL::IO::Color(image->getColor(coord).r() * 255,
+                                                       image->getColor(coord).g() * 255,
+                                                       image->getColor(coord).b() * 255);
+                }
+                if (pvmap.find(p2) == pvmap.end()) {
+                    pvmap[p2]        = m_nativeMeshPreference.add_vertex(p2);
+                    auto coord       = textureCoordArray->at(faces[i+2]);
+                    vcmap[pvmap[p2]] = CGAL::IO::Color(image->getColor(coord).r() * 255,
+                                                       image->getColor(coord).g() * 255,
+                                                       image->getColor(coord).b() * 255);
+                }
+                FaceIndex face_index =
+                    m_nativeMeshPreference.add_face(pvmap[p0], pvmap[p1], pvmap[p2]);
             }
         }
     }
