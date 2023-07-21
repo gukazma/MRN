7524c8251f209c08c968ef63a5af3e585aa65d6c
geekgary-x <18017158729@163.com>
2023-07-21 10:19:03 +0800

    ✨ feat: use env path

--- Tests/test0.cpp
+++ Tests/test0.cpp
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 #include <MRN/Mesh/Mesh.h>
+#include <boost/process.hpp>
+#include <boost/filesystem.hpp>
 TEST(MRN, OSGBRead)
 {
-    MRN::Mesh mesh("D:/data/合并根节点/test/Tile_+000_+014/Tile_+000_+014_15_0.osgb");
+    auto      env = boost::this_process::environment();
+    boost::filesystem::path MRNDataPath = env["MRNDATA"].to_string();
+    MRN::Mesh mesh(MRNDataPath/"osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb");
     mesh.write("D:/data/test.ply");
+    mesh.write(MRNDataPath /"ouput/read.ply");
 }
