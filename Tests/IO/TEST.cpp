85a79f96a37c0ae99201d2b267f3defc59683502
geekgary-x <18017158729@163.com>
2023-08-04 10:05:53 +0800

    ✨ feat: vcg simplify

--- Tests/IO/TEST.cpp
+++ Tests/IO/TEST.cpp
@@ -1,15 +1,15 @@
-#include <gtest/gtest.h>
 #include <MRN/Mesh/Mesh.h>
-#include <boost/process.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/process.hpp>
+#include <gtest/gtest.h>
 TEST(MRN, OSGBRead)
 {
-    auto      env = boost::this_process::environment();
+    auto                    env         = boost::this_process::environment();
     boost::filesystem::path MRNDataPath = env["MRNDATA"].to_string();
-    MRN::Mesh mesh(MRNDataPath/"osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb");
-    //std::cout << MRNDataPath / "osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb" << std::endl; 
+    MRN::Mesh               mesh(MRNDataPath / "osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb");
+    // std::cout << MRNDataPath / "osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb" << std::endl;
     mesh.write("D:/data/test.ply");
-    mesh.write(MRNDataPath /"output/read.ply");
+    mesh.write(MRNDataPath / "output/read.ply");
 }
 
 TEST(MRN, OSGBWrite)
--- Tests/Join/TEST.cpp
+++ Tests/Join/TEST.cpp
@@ -1,19 +1,26 @@
-#include <gtest/gtest.h>
-#include <iostream>
-#include <boost/process.hpp>
-#include <boost/filesystem.hpp>
-#include <MRN/Mesh/Types.h>
 #include <MRN/Mesh/Mesh.h>
-#include <CGAL/Polygon_mesh_processing/bbox.h>
-#include <CGAL/Bbox_3.h>
-#include <unordered_map>
-#include <fstream>
+#include <boost/filesystem.hpp>
+#include <boost/process.hpp>
+#include <gtest/gtest.h>
+TEST(MRN, JOIN)
+{
+    std::cout << "Test1" << std::endl;
+    auto                    env         = boost::this_process::environment();
+    boost::filesystem::path MRNDataPath = env["MRNDATA"].to_string();
+    MRN::Mesh               mesh14(MRNDataPath / "osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb");
+    MRN::Mesh               mesh15(MRNDataPath / "osgb/Tile_+000_+015/Tile_+000_+015_15_0.osgb");
+    // mesh14.write(MRNDataPath / "output/tile14.ply");
+    // mesh15.write(MRNDataPath / "output/tile15.ply");
+    mesh14.join(mesh15);
+    mesh14.write(MRNDataPath / "output/tile14+15.ply");
+}
+
 TEST(MRN, JOINNEWVERSION)
 {
     std::cout << "JOIN NEW VERSION" << std::endl;
     auto                    env         = boost::this_process::environment();
     boost::filesystem::path MRNDataPath = env["MRNDATA"].to_string();
-    boost::filesystem::path outputDir      = MRNDataPath / "output";
+    boost::filesystem::path outputDir   = MRNDataPath / "output";
     MRN::Mesh               mesh0(MRNDataPath / "osgb/Tile_+000_+014/Tile_+000_+014_15_0.osgb");
     MRN::Mesh               mesh1(MRNDataPath / "osgb/Tile_+000_+015/Tile_+000_+015_15_0.osgb");
     MRN::SurfaceMesh&       surfacemesh0 = mesh0.getNativMesh();
