a1780adfd66cca5e0d2a2314c911dbbaebe5c191
geekgary-x <18017158729@163.com>
2023-08-15 10:08:33 +0800

    ✨ feat: tool button dir

--- Apps/MRN/Widgets/MRNWidget.cpp
+++ Apps/MRN/Widgets/MRNWidget.cpp
@@ -1,11 +1,16 @@
 #include "MRNWidget.h"
 #include "ui_MRNWidget.h"
-
+#include <QFileDialog>
 MRNWidget::MRNWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MRNWidget)
 {
     ui->setupUi(this);
+
+    connect(ui->toolButton, &QToolButton::clicked, [&]() { 
+            auto result = QFileDialog::getExistingDirectory();
+            ui->lineEdit_dataFolder->setText(result);
+        });
 }
 
 MRNWidget::~MRNWidget()
