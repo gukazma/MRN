bb8354ec5a9f4494e9bf98e003e6be8c8ae77b3c
gukazma <gukazma@proton.me>
2023-08-15 16:08:15 +0800

    ✨ feat: open folder

--- Apps/MRN/Widgets/MRNWidget.cpp
+++ Apps/MRN/Widgets/MRNWidget.cpp
@@ -1,6 +1,7 @@
 #include "MRNWidget.h"
 #include "ui_MRNWidget.h"
 #include <QFileDialog>
+#include <QDesktopServices>
 MRNWidget::MRNWidget(QWidget* parent)
     : QWidget(parent)
     , ui(new Ui::MRNWidget)
@@ -12,6 +13,7 @@ MRNWidget::MRNWidget(QWidget* parent)
         ui->lineEdit_dataFolder->setText(result);
     });
     connect(ui->pushButton_scan, SIGNAL(clicked(bool)), this, SLOT(slot_scan(bool)));
+    connect(ui->pushButton_openFolder, SIGNAL(clicked(bool)), this, SLOT(slot_openFolder(bool)));
 }
 
 void MRNWidget::slot_scan(bool)
@@ -19,8 +21,14 @@ void MRNWidget::slot_scan(bool)
     if (ui->comboBox_dataVendor->currentText() == "Samrt3D") {}
 }
 
+void MRNWidget::slot_openFolder(bool) {
+    if (ui->lineEdit_dataFolder->text().isEmpty()) return;
+    QUrl _url = QUrl::fromLocalFile(ui->lineEdit_dataFolder->text());
+    QDesktopServices::openUrl(_url);
+}
 
 MRNWidget::~MRNWidget()
 {
     delete ui;
 }
+
