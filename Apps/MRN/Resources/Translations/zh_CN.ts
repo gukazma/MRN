afcb45e78d7da0a0ad4c5a45eb2890885d01662a
gukazma <gukazma@proton.me>
2023-08-17 17:19:29 +0800

    ✨ feat: 初始化界面

---
+++ Apps/MRN/Resources/Translations/zh_CN.ts
@@ -0,0 +1,393 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>GenerateAirspaceDialog</name>
+    <message>
+        <source>Generate Airspace</source>
+        <translation type="obsolete">生成安全罩</translation>
+    </message>
+    <message>
+        <source>Input cloud path:</source>
+        <translation type="obsolete">点云输入路径：</translation>
+    </message>
+    <message>
+        <source>Output airspace path:</source>
+        <translation type="obsolete">安全罩输出路径：</translation>
+    </message>
+    <message>
+        <source>min-distance:</source>
+        <translation type="obsolete">最小扩散距离：</translation>
+    </message>
+    <message>
+        <source>Input cloud path</source>
+        <translation type="obsolete">点云输入路径</translation>
+    </message>
+    <message>
+        <source>Output airspace path</source>
+        <translation type="obsolete">安全罩输出路径</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="obsolete">确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">取消</translation>
+    </message>
+    <message>
+        <source>Generating airspace,please wait.</source>
+        <translation type="obsolete">正在生成安全罩，请稍等。</translation>
+    </message>
+</context>
+<context>
+    <name>GenerateMeshDialog</name>
+    <message>
+        <source>Input mesh path:</source>
+        <translation type="obsolete">输入网格路径</translation>
+    </message>
+    <message>
+        <source>Output poxymesh path:</source>
+        <translation type="obsolete">代理模型输出路径：</translation>
+    </message>
+    <message>
+        <source>Output airspace path:</source>
+        <translation type="obsolete">扩散模型输出路径：</translation>
+    </message>
+    <message>
+        <source>min-distance:</source>
+        <translation type="obsolete">最小扩散距离：</translation>
+    </message>
+    <message>
+        <source>Generate poxymesh</source>
+        <translation type="obsolete">生成代理模型</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="obsolete">确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">取消</translation>
+    </message>
+    <message>
+        <source>Input cloud path:</source>
+        <translation type="obsolete">点云输入路径：</translation>
+    </message>
+    <message>
+        <source>Output poxymesh path</source>
+        <translation type="obsolete">代理模型输出路径</translation>
+    </message>
+    <message>
+        <source>Output airspace path</source>
+        <translation type="obsolete">扩散模型输出路径</translation>
+    </message>
+    <message>
+        <source>Input cloud path</source>
+        <translation type="obsolete">输入点云路径</translation>
+    </message>
+    <message>
+        <source>Generating poxymesh,please wait.</source>
+        <translation type="obsolete">正在生成代理模型，请稍等。</translation>
+    </message>
+</context>
+<context>
+    <name>GenerateTrajectoryDialog</name>
+    <message>
+        <source>Generate trajectory</source>
+        <translation type="obsolete">生成初始轨迹</translation>
+    </message>
+    <message>
+        <source>Input cloud path:</source>
+        <translation type="obsolete">代理模型输入路径：</translation>
+    </message>
+    <message>
+        <source>Output trajectory path:</source>
+        <translation type="obsolete">初始轨迹输出路径：</translation>
+    </message>
+    <message>
+        <source>altitude:</source>
+        <translation type="obsolete">高度：</translation>
+    </message>
+    <message>
+        <source>elevation:</source>
+        <translation type="obsolete">海拔高度：</translation>
+    </message>
+    <message>
+        <source>angles:</source>
+        <translation type="obsolete">摄影姿态角：</translation>
+    </message>
+    <message>
+        <source>forward-overlap:</source>
+        <translation type="obsolete">前向重叠度：</translation>
+    </message>
+    <message>
+        <source>side-overlap:</source>
+        <translation type="obsolete">侧向重叠度：</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="obsolete">确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">取消</translation>
+    </message>
+    <message>
+        <source>Input cloud path</source>
+        <translation type="obsolete">代理模型输入路径</translation>
+    </message>
+    <message>
+        <source>Output trajectory path</source>
+        <translation type="obsolete">初始轨迹输出路径</translation>
+    </message>
+    <message>
+        <source>Input mesh path:</source>
+        <translation type="obsolete">代理模型输入路径：</translation>
+    </message>
+    <message>
+        <source>Input mesh path</source>
+        <translation type="obsolete">代理模型输入路径</translation>
+    </message>
+    <message>
+        <source>Generating trajectories,please wait.</source>
+        <translation type="obsolete">正在生成初始轨迹，请稍等。</translation>
+    </message>
+    <message>
+        <source>Input airspace path:</source>
+        <translation type="obsolete">安全罩输入路径：</translation>
+    </message>
+    <message>
+        <source>Input airspace path</source>
+        <translation type="obsolete">输入安全罩路径</translation>
+    </message>
+</context>
+<context>
+    <name>MRNWidget</name>
+    <message>
+        <source>Merge root node</source>
+        <translation type="unfinished">合并根节点</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data Vendor:</source>
+        <translation type="unfinished">数据生产商：</translation>
+    </message>
+    <message>
+        <source>Scan</source>
+        <translation type="unfinished">扫描文件夹</translation>
+    </message>
+    <message>
+        <source>Begin</source>
+        <translation type="unfinished">开始合并</translation>
+    </message>
+    <message>
+        <source>Open folder</source>
+        <translation type="unfinished">打开结果</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Smart3D UAV Plan</source>
+        <translation type="obsolete">Smart3D 无人机路径规划</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="obsolete">文件</translation>
+    </message>
+    <message>
+        <source>Generate UAV Trajectory</source>
+        <translation type="obsolete">生成无人机轨迹</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="obsolete">帮助</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation type="obsolete">切换语言</translation>
+    </message>
+    <message>
+        <source>Open Mesh</source>
+        <translation type="obsolete">打开模型文件</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="obsolete">英语</translation>
+    </message>
+    <message>
+        <source>Chinese</source>
+        <translation type="obsolete">中文</translation>
+    </message>
+    <message>
+        <source>Open mesh</source>
+        <translation type="obsolete">打开模型</translation>
+    </message>
+    <message>
+        <source>Import trajectory</source>
+        <translation type="obsolete">输入轨迹</translation>
+    </message>
+    <message>
+        <source>Generate Mesh</source>
+        <translation type="obsolete">生成代理模型</translation>
+    </message>
+    <message>
+        <source>Generate poxymesh</source>
+        <translation type="obsolete">生成代理模型</translation>
+    </message>
+    <message>
+        <source>Generate trajectory</source>
+        <translation type="obsolete">生成初始轨迹</translation>
+    </message>
+    <message>
+        <source>Optimize trajectory</source>
+        <translation type="obsolete">优化初始轨迹</translation>
+    </message>
+    <message>
+        <source>Scale</source>
+        <translation type="obsolete">缩放</translation>
+    </message>
+    <message>
+        <source>Generate airspace</source>
+        <translation type="obsolete">生成安全罩</translation>
+    </message>
+    <message>
+        <source>New Project</source>
+        <translation type="obsolete">新建工程</translation>
+    </message>
+    <message>
+        <source>Save Project</source>
+        <translation type="obsolete">保存工程</translation>
+    </message>
+    <message>
+        <source>Open Project</source>
+        <translation type="obsolete">打开工程</translation>
+    </message>
+    <message>
+        <source>Project Tree:</source>
+        <translation type="obsolete">工程树：</translation>
+    </message>
+</context>
+<context>
+    <name>MeshTreeWidgetItem</name>
+    <message>
+        <source>Generate poxy mesh</source>
+        <translation type="obsolete">生成代理模型</translation>
+    </message>
+    <message>
+        <source>Generate trajectories</source>
+        <translation type="obsolete">生成轨迹</translation>
+    </message>
+</context>
+<context>
+    <name>NewProjectDialog</name>
+    <message>
+        <source>New Project</source>
+        <translation type="obsolete">新建工程</translation>
+    </message>
+</context>
+<context>
+    <name>OptimizeTrajectoryDialog</name>
+    <message>
+        <source>Scale</source>
+        <translation type="obsolete">缩放</translation>
+    </message>
+    <message>
+        <source>Optimize trajectory</source>
+        <translation type="obsolete">优化初始轨迹</translation>
+    </message>
+    <message>
+        <source>Input mesh path:</source>
+        <translation type="obsolete">代理模型输入路径：</translation>
+    </message>
+    <message>
+        <source>Input Initial trajectory path:</source>
+        <translation type="obsolete">初始轨迹输入路径：</translation>
+    </message>
+    <message>
+        <source>Input cloud path:</source>
+        <translation type="obsolete">点云输入路径：</translation>
+    </message>
+    <message>
+        <source>Input airspace path:</source>
+        <translation type="obsolete">安全罩输入路径：</translation>
+    </message>
+    <message>
+        <source>Output Optimized trajectory trajectory path:</source>
+        <translation type="obsolete">优化后的轨迹输出路径：</translation>
+    </message>
+    <message>
+        <source>max-distance:</source>
+        <translation type="obsolete">与表面的最大距离：</translation>
+    </message>
+    <message>
+        <source>min-distance:</source>
+        <translation type="obsolete">与表面的最小距离：</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="vanished">确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">取消</translation>
+    </message>
+    <message>
+        <source>Input Initial trajectory path</source>
+        <translation type="obsolete">初始轨迹输入路径</translation>
+    </message>
+    <message>
+        <source>Input cloud path</source>
+        <translation type="obsolete">点云输入路径</translation>
+    </message>
+    <message>
+        <source>Input mesh path</source>
+        <translation type="obsolete">代理模型输入路径</translation>
+    </message>
+    <message>
+        <source>Input Airspace path</source>
+        <translation type="obsolete">安全罩输入路径</translation>
+    </message>
+    <message>
+        <source>Output Optimized trajectory trajectory path</source>
+        <translation type="obsolete">优化后的轨迹输出路径</translation>
+    </message>
+    <message>
+        <source>Optimizing trajectories,please wait.</source>
+        <translation type="obsolete">正在优化初始轨迹，请稍等。</translation>
+    </message>
+    <message>
+        <source>max-iters:</source>
+        <translation type="obsolete">最大循环次数：</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressBar</name>
+    <message>
+        <source>Please wait.</source>
+        <translation type="obsolete">请稍等。</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectTreeWidgetItem</name>
+    <message>
+        <source>Poxy Meshs</source>
+        <translation type="obsolete">代理模型</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectWidget</name>
+    <message>
+        <source>Project tree</source>
+        <translation type="obsolete">工程树</translation>
+    </message>
+    <message>
+        <source>Property</source>
+        <translation type="obsolete">属性</translation>
+    </message>
+</context>
+</TS>
--- Apps/MRN/main.cpp
+++ Apps/MRN/main.cpp
@@ -2,11 +2,26 @@
 #include <QApplication>
 #include <iostream>
 #include <thread>
+#include <QTranslator>
+#include <QFile>
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
+    // initialize language
+    QString   language = "zh_CN";
+
+    QTranslator* translator = new QTranslator;
+    translator->load(":/Translations/" + language + ".qm");
+    app.installTranslator(translator);
+
+    // initialize style
+    QFile file(":/style.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+    qApp->setStyleSheet(styleSheet);
+
     MRNWidget mrnwidget;
-    mrnwidget.show();
+    mrnwidget.showMaximized();
     return app.exec();
 }
